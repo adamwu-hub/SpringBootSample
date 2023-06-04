a246514654283143deae93af5e78e2a22dc5a163
aw <wj.group@hotmail.com>
2023-06-04 23:19:06 +1000

    Demo the use of multiple config files and the use of HttpClient

--- src/main/java/com/example/core/service/MessageService.java
+++ src/main/java/com/example/core/service/MessageService.java
@@ -5,6 +5,12 @@ import com.example.domain.Message;
 import com.example.domain.MyAppProperties;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
 @Service
 public class MessageService implements IMessageService {
 
@@ -16,6 +22,31 @@ public class MessageService implements IMessageService {
     
     @Override
     public Message getMessage(String content) {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        URI httpURI;
+        try {
+            httpURI = new URI("http://jsonplaceholder.typicode.com/posts/1");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        HttpRequest request = HttpRequest.newBuilder(httpURI).build();
+
+        CompletableFuture<String> futureResponse = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                /*.thenAccept(resp -> {
+                    System.out.println("Got pushed response " + resp.uri());
+                    System.out.println("Response status code: " + resp.statusCode());
+                    System.out.println("Response body: " + resp.body());
+                })*/
+                .thenApply(rest -> rest.body());
+
+        var result = futureResponse.join();
+
+        System.out.println("futureResponse is " + futureResponse);
+
+        System.out.println("after future response: " + result);
+
         return new Message(String.format("%s %s %s",
                 _appProperties.getName(),
                 _appProperties.getVersion(),
